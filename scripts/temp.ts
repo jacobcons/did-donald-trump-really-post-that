975d37f5b9cca8428d8ccf9314420074a26c1038
Jacob <jacobcons@gmail.com>
2024-11-23 18:14:40 +0000

    new kim jong un audio clip

--- scripts/temp.ts
+++ scripts/temp.ts
@@ -12,3 +12,10 @@ let [real, fake] = await Promise.all([
   readJSON('./real-tweets.json'),
   readJSON('./fake-tweets.json'),
 ]);
+
+for (let i = 0; i < real.length; i++) {
+  if (1) {
+    console.log(fake[i]);
+    console.log(i);
+  }
+}
